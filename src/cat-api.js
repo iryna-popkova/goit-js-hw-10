d5dd049bc7325138fe61c83bff51b2815f6513bf
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2023-09-23 09:45:14 +0300

    start

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,26 +3,26 @@ import axios from "axios";
 axios.defaults.headers.common["x-api-key"] = "live_9ZD2TQX2QKLus1Y8wIzEJaT6YqXtRqwgygQUSwYpqKJNfP95y1L7SL3pWp624OxV";
 
 export function fetchBreeds() {
-
-return fetch("https://api.thecatapi.com/v1/breeds?x-api-key").then(
-    (response) => {
-      if (!response.ok) {
-        throw new Error(response.status);
+  return axios.get("https://api.thecatapi.com/v1/breeds")
+    .then((response) => {
+      return response.data;
       }
-      return response.json();
+    )
+    .catch((error) => {
+      return new Error('Oops! Something went wrong! Try reloading the page!');
     }
-  );
+  )
+}
 
-};
 
 export function fetchCatByBreed(breedId) {
-return fetch("https://api.thecatapi.com/v1/images/search?x-api-key&breed_ids").then(
-    (response) => {
-      if (!response.ok) {
-        throw new Error(response.status);
+  return axios.get( `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then((response) => {
+      return response.data[0];
       }
-      return response.json();
+    )
+    .catch((error) => {
+      return new Error('Oops! Something went wrong! Try reloading the page!');
     }
-  );
-
-};
+  )
+}
--- src/index.js
+++ src/index.js
@@ -6,4 +6,30 @@ import './styles.css';
 const breedSelector = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
-const catInfo = document.querySelector('.cat-info')
+const catInfo = document.querySelector('.cat-info');
+
+fetchBreeds()
+  .then(breedsArray => {
+    const markup = breedsArray.map(({ id, name }) => {
+      return `<option value = "${id}">${name}</option>`
+    }).join('');
+    breedSelector.insertAdjacentHTML('afterbegin', markup);
+  }
+ )
+  .catch(error => console.error(error));
+
+breedSelector.addEventListener('change', onSelect);
+
+function onSelect(event) {
+  const breedId = event.target.value;
+  fetchCatByBreed(breedId)
+    .then(breedInfo => {
+  const catDscr = `${breedInfo.breeds.name}+
+${breedInfo.breeds.description}+
+${breedInfo.breeds.temperament}`;
+      catDscr.insertAdjacentHTML('afterbegin', catInfo )
+    }
+
+  )
+}
+
